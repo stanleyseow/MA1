e24fc4e254ad7db3e0dc24dfd858e9b87c65e758
Stanley Seow <stanleyseow@gmail.com>
2022-12-16 02:41:42 +0800

    Added game with knife object

---
+++ ToRescue-objectClasses/knife.js
@@ -0,0 +1,53 @@
+class Knife extends Phaser.Physics.Arcade.Sprite {
+    constructor ( scene, x, y) {
+        super(scene, x, y, 'knifeImg')
+
+        // Create the knife object and add to existing scene
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+    }
+
+    // Shoot method
+    shoot(x,y, dir) {
+        //console.log("Shoot", this, x, y, dir)
+        this.speed = 500
+        this.dir = dir;
+        this.play('knifeAnim');
+
+        switch (this.dir) {
+            // 0 - left, 1 - right, 2, up - 3 - down
+            case 0 : 
+                this.setVelocityX(-this.speed)
+                break;
+            case 1 : 
+                this.setVelocityX(this.speed)
+                break;
+            case 2 : 
+                this.setVelocityY(-this.speed)
+                break;
+            case 3 : 
+                this.setVelocityY(this.speed)
+                break;
+            default:
+                break;    
+        }
+    }
+
+    // destroy knife
+    destroy() {
+        this.disableBody(true,true)
+    }
+
+    // preupdate 
+    preUpdate(time, delta) {
+        super.preUpdate(time, delta);
+
+        // Reset the knifes  when it reaches end of screen
+        if (this.x < 0 || this.x > 1200) {
+            this.disableBody(true,true)
+            
+        }
+    }
+
+
+}
