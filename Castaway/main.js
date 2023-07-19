7e1aadb6d47dd48c371de621286ac348b455ee9a
Stanley Seow <stanleyseow@gmail.com>
2023-07-19 13:16:31 +0800

    Castaway Skip to world scene

--- Castaway/main.js
+++ Castaway/main.js
@@ -9,22 +9,22 @@ class main extends Phaser.Scene {
 
   init(data) {
     this.playerPos = data.playerPos;
-}
+  }
 
   preload() {
     {
       this.load.atlas('right', 'assets/hiro walk.png',
-                                   'assets/hiro walk.json')
+        'assets/hiro walk.json')
       this.load.atlas('hiro attack', 'assets/hiro attack.png',
-                                     'assets/hiro attack.json')
+        'assets/hiro attack.json')
       this.load.atlas('down', 'assets/hiro front.png',
-                                    'assets/hiro front.json')
+        'assets/hiro front.json')
       this.load.atlas('up', 'assets/hiro back.png',
-                                   'assets/hiro back.json')
+        'assets/hiro back.json')
 
       this.load.image('main', 'assets/intro.png');
-      this.load.audio('islandMusic','assets/island.wav');
-   }
+      this.load.audio('islandMusic', 'assets/island.wav');
+    }
   }
 
   create() {
@@ -33,7 +33,7 @@ class main extends Phaser.Scene {
     // Add any sound and music here
     // ( 0 = mute to 1 is loudest )
     //this.music = this.sound.add('bgMusic').setVolume(0.3) // 10% volume
-    window.introMusic = this.sound.add('islandMusic', { loop:true,}).setVolume(0.1);
+    window.introMusic = this.sound.add('islandMusic', { loop: true, }).setVolume(0.1);
     window.introMusic.play();
 
     // this.bgmSnd = this.sound.add('islandMusic').setVolume(0.2);
@@ -48,72 +48,72 @@ class main extends Phaser.Scene {
 
     //animation for Hiro
     this.anims.create({
-      key:'left',
+      key: 'left',
       frames: [
-          {key:'right', frame:'walk 01'},
-          {key:'right', frame:'walk 02'},
-          {key:'right', frame:'walk 03'},
-          {key:'right', frame:'walk 04'},
+        { key: 'right', frame: 'walk 01' },
+        { key: 'right', frame: 'walk 02' },
+        { key: 'right', frame: 'walk 03' },
+        { key: 'right', frame: 'walk 04' },
       ],
       frameRate: 6,
       repeat: -1
-  })
+    })
 
-  this.anims.create({
-      key:'right',
-      frames:[
-          {key:'right', frame:'walk 05'},
-          {key:'right', frame:'walk 06'},
-          {key:'right', frame:'walk 07'},
-          {key:'right', frame:'walk 08'},
+    this.anims.create({
+      key: 'right',
+      frames: [
+        { key: 'right', frame: 'walk 05' },
+        { key: 'right', frame: 'walk 06' },
+        { key: 'right', frame: 'walk 07' },
+        { key: 'right', frame: 'walk 08' },
       ],
       frameRate: 6,
       repeat: -1
-  })
+    })
 
-  this.anims.create({
-      key:'down',
-      frames:[
-          {key:'down',frame:'hiro front 01'},
-          {key:'down',frame:'hiro front 02'},
-          {key:'down',frame:'hiro front 03'},
+    this.anims.create({
+      key: 'down',
+      frames: [
+        { key: 'down', frame: 'hiro front 01' },
+        { key: 'down', frame: 'hiro front 02' },
+        { key: 'down', frame: 'hiro front 03' },
       ],
       frameRate: 6,
       repeat: -1
-  })
+    })
 
-  this.anims.create({
-      key:'up',
-      frames:[
-          {key:'up',frame:'hiro back 01'},
-          {key:'up',frame:'hiro back 02'},
-          {key:'up',frame:'hiro back 03'},
+    this.anims.create({
+      key: 'up',
+      frames: [
+        { key: 'up', frame: 'hiro back 01' },
+        { key: 'up', frame: 'hiro back 02' },
+        { key: 'up', frame: 'hiro back 03' },
       ],
       frameRate: 5,
       repeat: -1
-  })
+    })
 
-  this.anims.create({
-    key:'leftAttack',
-    frames:[
-        {key:'hiro attack',frame:'hiro attack 01'},
-        {key:'hiro attack',frame:'hiro attack 02'},
-        {key:'hiro attack',frame:'hiro attack 03'},
-        {key:'hiro attack',frame:'hiro attack 04'},
-    ],
-    frameRate: 10,
-    repeat: 0
-})
+    this.anims.create({
+      key: 'leftAttack',
+      frames: [
+        { key: 'hiro attack', frame: 'hiro attack 01' },
+        { key: 'hiro attack', frame: 'hiro attack 02' },
+        { key: 'hiro attack', frame: 'hiro attack 03' },
+        { key: 'hiro attack', frame: 'hiro attack 04' },
+      ],
+      frameRate: 10,
+      repeat: 0
+    })
 
-  this.anims.create({
-  key:'idle',
-  frames:[
-      {key:'right', frame:'walk 01'},
-      {key:'right', frame:'walk 02'},
-  ],
-  frameRate: 5,
-  repeat: -1
-})
+    this.anims.create({
+      key: 'idle',
+      frames: [
+        { key: 'right', frame: 'walk 01' },
+        { key: 'right', frame: 'walk 02' },
+      ],
+      frameRate: 5,
+      repeat: -1
+    })
 
     // Check for spacebar or any key here
     var spaceDown = this.input.keyboard.addKey("SPACE");
@@ -127,65 +127,74 @@ class main extends Phaser.Scene {
     spaceDown.on(
       "down",
       function () {
-      console.log("Jump to story1 scene");
-      this.scene.start("story1");
-      },
-      this
-    );
-  
-    // on enter to jump into world
-      enterDown.on (
-        "down", function (){
+
         console.log("jump to world");
         window.introMusic.stop();
 
-      let playerPos = {};
-      playerPos.x = 50;
-      playerPos.y = 150;
-      playerPos.dir = "right";
-      this.scene.start("world", { player: playerPos });
-      },this);
+        let playerPos = {};
+        playerPos.x = 50;
+        playerPos.y = 150;
+        playerPos.dir = "right";
+        this.scene.start("world", { player: playerPos });
+      },
+      this
+    );
 
-      // Jump to room1
-      key1.on (
-        "down", function (){
+    // on enter to jump into world
+    enterDown.on(
+      "down", function () {
+        console.log("jump to world");
+        window.introMusic.stop();
+
+        let playerPos = {};
+        playerPos.x = 50;
+        playerPos.y = 150;
+        playerPos.dir = "right";
+        this.scene.start("world", { player: playerPos });
+      },
+      this
+    );
+
+    // Jump to room1
+    key1.on(
+      "down", function () {
         console.log("jump to room1");
         window.introMusic.stop();
-  
+
         let playerPos = {};
         playerPos.x = 340;
         playerPos.y = 526;
         playerPos.dir = "up";
-        this.scene.start("room1",{playerPos:playerPos});
-      },this);
+        this.scene.start("room1", { playerPos: playerPos });
+      }, this);
 
-      // Jump to room2
-      key2.on (
-        "down", function (){
+    // Jump to room2
+    key2.on(
+      "down", function () {
         console.log("jump to room2");
-        
+
         window.introMusic.stop();
-  
+
         let playerPos = {};
         playerPos.x = 353;
         playerPos.y = 677;
         playerPos.dir = "up";
-        this.scene.start("room2",{playerPos:playerPos});
-      },this);
+        this.scene.start("room2", { playerPos: playerPos });
+      }, this);
 
-        // Jump to room2
-        key3.on (
-          "down", function (){
-          console.log("jump to room3");
-          
-          window.introMusic.stop();
-    
-          let playerPos = {};
-          playerPos.x = 200;
-          playerPos.y = 677;
-          playerPos.dir = "up";
-          this.scene.start("room3",{playerPos:playerPos});
-        },this);
+    // Jump to room2
+    key3.on(
+      "down", function () {
+        console.log("jump to room3");
+
+        window.introMusic.stop();
+
+        let playerPos = {};
+        playerPos.x = 200;
+        playerPos.y = 677;
+        playerPos.dir = "up";
+        this.scene.start("room3", { playerPos: playerPos });
+      }, this);
 
     // Add any text in the main page
     this.add.text(190, 350, "PRESS SPACEBAR TO CONTINUE", {
